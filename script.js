6fde083a7129408b1deefc91d786c7ecba0fc691
Maryam <maryamdevelopp@gmail.com>
2017-08-03 23:19:51 +0200

    modification de activite.html et de script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 alert('ceci est un fichier js pour la première activité');
 console.log('texte à afficher par la console du navigateur');
+var bloc=document.getElementById("contenu");
+var paragraphe=document.createElement("p");
+paragraphe.textContent="ceci est un texte ajouté en utilisant javascript";
+bloc.appendChild(paragraphe);
